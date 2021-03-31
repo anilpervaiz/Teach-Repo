cb1a2bd1f889b0b053503c45df5105f09bc49f9c
Rahim Khalid Iqbal <jessanirahim@gmail.com>
2021-03-31 19:08:14 +0200

    UI fixes for document cell

--- Teachers/App/Scenes/Authentication/Subjects/Cells/DocumentCell.swift
+++ Teachers/App/Scenes/Authentication/Subjects/Cells/DocumentCell.swift
@@ -6,5 +6,11 @@ import Foundation
 import UIKit
 
 class DocumentCell: UITableViewCell, ReusableView {
+    @IBOutlet weak var docThumbnailImageView: UIImageView!
     
+    @IBOutlet weak var docNameLabel: UILabel!
+    @IBOutlet weak var docSizeAndDateLabel: UILabel!
+    
+    @IBAction func moreOptionsTapped(_ sender: Any) {
+    }
 }
