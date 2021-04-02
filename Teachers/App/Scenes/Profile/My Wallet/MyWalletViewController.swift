80eafbe1985539de3d90d9da99dc64b6cc9af517
Rahim Khalid Iqbal <jessanirahim@gmail.com>
2021-04-02 14:02:51 +0500

    MYWallet screenWIP

---
+++ Teachers/App/Scenes/Profile/My Wallet/MyWalletViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyWalletViewController.swift
+//  Teachers
+//
+//  Created by Rahim on 31/03/2021.
+//
+
+import UIKit
+
+class MyWalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
